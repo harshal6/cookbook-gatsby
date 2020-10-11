3bda15a6e71c6c1e4e586319b51ce610afee0677
Harshal <harshal.a.pradhan@gmail.com>
2020-10-12 04:30:35 +0530

    Updated Product logic.

--- src/pages/buyCookbook.js
+++ src/pages/buyCookbook.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Product from '../components/product';
 
 const IndexPage = () => {
-    const Bookitem = {price: process.env.BOOK_WITH_IPAD_ITEM, quantity: 1};
+    const Bookitem = {price: process.env.BOOK_ITEM, quantity: 1};
     const ipad = {price: process.env.BOOK_WITH_IPAD_ITEM, quantity: 1}
     return (
     <div>
-        <Product label ={"Buy Book And Ipad at $120"} lineItems = {[Bookitem, ipad]} />
+        <Product label ={"Buy Book And Ipad"} lineItems = {[ipad]} />
         <Product label ={"Buy Book"} lineItems = {[Bookitem]}/>
       </div>
     )
