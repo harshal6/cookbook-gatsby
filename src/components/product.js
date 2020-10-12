5351ebb4e0073416121b774053e218f7057d05cd
Harshal <harshal.a.pradhan@gmail.com>
2020-10-13 01:22:03 +0530

    Updated the environment variable.

--- src/components/product.js
+++ src/components/product.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 class Product extends Component {
   componentDidMount() {
-    this.stripe = window.Stripe(${process.env.STRIPE_PUB_KEY})
+    this.stripe = window.Stripe({process.env.STRIPE_PUB_KEY})
   }
   render() {
-      console.log(${process.env.STRIPE_PUB_KEY});
+      console.log({process.env.STRIPE_PUB_KEY});
       return (
       <form
         onSubmit={event => {
--- src/pages/buyCookbook.js
+++ src/pages/buyCookbook.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Product from '../components/product';
 
 const IndexPage = () => {
-    const Bookitem = {price: ${process.env.BOOK_ITEM}, quantity: 1};
+    const Bookitem = {price: {process.env.BOOK_ITEM}, quantity: 1};
     return (
     <div>
         <Product label ={"Buy Book"} lineItems = {[Bookitem]}/>
