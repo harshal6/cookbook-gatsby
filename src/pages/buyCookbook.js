75c5f61097b90948f654a46a43dbe19ff4caa359
Harshal <harshal.a.pradhan@gmail.com>
2020-10-13 01:31:28 +0530

    test

--- src/pages/buyCookbook.js
+++ src/pages/buyCookbook.js
@@ -5,6 +5,7 @@ const IndexPage = () => {
     const Bookitem = {price: `${process.env.BOOK_ITEM}`, quantity: 1};
     return (
     <div>
+        {process.env.BOOK_ITEM}
         <Product label ={"Buy Book"} lineItems = {[Bookitem]}/>
       </div>
     )
