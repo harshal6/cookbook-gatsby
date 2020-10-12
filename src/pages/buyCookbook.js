5ead03ae06985cf32de661a9b3af2888c5413b16
Harshal <harshal.a.pradhan@gmail.com>
2020-10-13 01:46:16 +0530

    test2

--- src/pages/buyCookbook.js
+++ src/pages/buyCookbook.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Product from '../components/product';
 
 const IndexPage = () => {
-    const Bookitem = {price: `${process.env.BOOK_ITEM}`, quantity: 1};
+    const Bookitem = {price: process.env.STRIPE_PUB_KEY, quantity: 1};
     return (
     <div>
-        {process.env.BOOK_ITEM}
         <Product label ={"Buy Book"} lineItems = {[Bookitem]}/>
       </div>
     )
